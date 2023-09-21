288f765e0bbc3566b635294201e827b6df9b9b70
LHentges <Lance.Hentges@imm.ox.ac.uk>
2023-09-21 13:59:53 +0100

    Update candidate peak calls

--- lotron2.py
+++ lotron2.py
@@ -5,6 +5,7 @@ from scipy.stats import poisson
 import pandas as pd
 import pyranges as pr
 import natsort
+import keras
 
 
 def average_array(array, window=None):
@@ -167,6 +168,11 @@ def calculate_p_values(observed, expected):
 class BigwigData:
     def __init__(self, bigwig_file):
         self.bigwig_file = bigwig_file
+
+    def get_total_coverage(self):
+        pybw = pyBigWig.open(self.bigwig_file)
+        return pybw.header()['sumData']
+
 
     def get_chrom_info(self, chrom_name):
         pybw = pyBigWig.open(self.bigwig_file)
@@ -334,3 +340,161 @@ class BigwigData:
 
         
 
+################# helper functions to make backward compatible with LanceOtron #####################
+
+
+
+
+############### From original LanceOtron module #####################
+
+
+
+def calculate_poisson_pvalue_array(enrichment_list, lambda_list):
+    pvalue_array = np.zeros((len(enrichment_list), len(lambda_list)))
+    for i, enrichment in enumerate(enrichment_list):
+            for j, lambda_val in enumerate(lambda_list):
+                with np.errstate(divide='ignore'):
+                    pvalue_array[i][j] = -1*np.log10(1-poisson.cdf(enrichment, lambda_val))
+    pvalue_array = np.reshape(pvalue_array, (1,-1))
+    pvalue_array_infs_indicies = np.where(np.isinf(pvalue_array))
+    pvalue_array[pvalue_array_infs_indicies] = 999
+    return np.nan_to_num(pvalue_array)
+
+
+
+def extract_signal_wide_and_deep_chrom(chrom_cov_array, coord_list, seq_depth):
+    features = 2000
+    X_deep = np.zeros((len(coord_list), features))
+    lambda_cov = 100000
+    lambda_count = 10
+    interval = int((lambda_cov/lambda_count))
+    length_list = [interval*(x+1) for x in range(lambda_count)]
+    X_wide = np.zeros((len(coord_list), lambda_count+2))
+    chrom_len = len(chrom_cov_array)
+    chrom_mean = chrom_cov_array.mean()
+    for i, coord in enumerate(coord_list):
+        pvalue_array = np.zeros(lambda_count+1)
+        start_coord = coord[0]
+        end_coord = coord[1]
+        region_length = end_coord-start_coord
+        max_height = chrom_cov_array[start_coord:end_coord].max()
+        if (max_height>0):
+            region_start = start_coord+int(region_length/2)-int(features/2)
+            coverage_length = len(chrom_cov_array[region_start:region_start+features])
+            if coverage_length==features:
+                X_deep[i] = chrom_cov_array[region_start:region_start+features]
+            else:
+                if (chrom_len<features):
+                    pad = int((features-chrom_len)/2)
+                    X_deep[i][pad:pad+chrom_len] = chrom_cov_array
+                elif (region_start<0):
+                    region_start = 0
+                    X_deep[i] = chrom_cov_array[region_start:region_start+features]
+                else:
+                    region_start = chrom_len-features
+                    X_deep[i] = chrom_cov_array[region_start:region_start+features]
+            # logic for edge cases - if not edge, pull out whole area and use array slices to find means
+            # else pull out larger and larger pieces:
+            #     if beyond bound two-sided use last mean; one sided, use signal until bound
+            lambda_list = [chrom_mean/seq_depth]
+            if (start_coord+int(region_length/2)-lambda_cov>=0) and (start_coord+int(region_length/2)+lambda_cov<=chrom_len):
+                lambda_cov_array = chrom_cov_array[start_coord+int(region_length/2)-lambda_cov:start_coord+int(region_length/2)+lambda_cov]
+                for length in length_list:
+                    pad = lambda_cov-length
+                    if pad==0:
+                        lambda_list.append(lambda_cov_array.mean())
+                    else:
+                        lambda_list.append(lambda_cov_array[pad:-pad].mean())
+            else:
+                for j, length in enumerate(length_list):
+                    if (start_coord+int(region_length/2)-length>=0) or (start_coord+int(region_length/2)+length<=chrom_len):
+                        if (start_coord+int(region_length/2)-length>=0):
+                            start_coord_for_mean = start_coord+int(region_length/2)-length
+                        else:
+                            start_coord_for_mean = 0
+                        if (start_coord+int(region_length/2)+length<=chrom_len):
+                            end_coord_for_mean = start_coord+int(region_length/2)
+                        else:
+                            end_coord_for_mean = chrom_len
+                        lambda_entry = chrom_cov_array[start_coord_for_mean:end_coord_for_mean].mean()
+                        lambda_list.append(lambda_entry)
+                    else:
+                        lambda_list.append(lambda_list[j]) 
+            enrichment_list  = [max_height]
+            pvalue_array = calculate_poisson_pvalue_array(enrichment_list, lambda_list)
+        X_wide[i] = np.append(pvalue_array, seq_depth)
+    return X_wide, X_deep
+
+
+def build_model(model_weights_file):
+    deep_dense_size = 10
+    dropout_rate = 0.5
+    first_filter_num = 70
+    first_filter_size = 9
+    hidden_filter_num = 120
+    hidden_filter_size = 6
+    learning_rate = 0.0001
+    wide_and_deep_dense_size = 70
+
+    input_wide = keras.layers.Input(shape=(12, 1))
+    wide_model = keras.layers.Flatten()(input_wide)
+    input_deep = keras.layers.Input((2000, 1))
+    deep_model = input_deep
+    # deep model first conv layer 
+    deep_model = keras.layers.Convolution1D(first_filter_num, kernel_size=first_filter_size, padding='same')(deep_model)
+    deep_model = keras.layers.BatchNormalization()(deep_model)
+    #deep_model = keras.layers.Activation('relu')(deep_model)
+    deep_model = keras.layers.LeakyReLU()(deep_model)
+    # deep model - 4 conv blocks
+    deep_model = keras.layers.Convolution1D(hidden_filter_num, kernel_size=hidden_filter_size, padding='same')(deep_model)
+    deep_model = keras.layers.BatchNormalization()(deep_model)
+    #deep_model = keras.layers.Activation('relu')(deep_model)
+    deep_model = keras.layers.LeakyReLU()(deep_model)
+    deep_model = keras.layers.MaxPool1D(pool_size=2)(deep_model)
+
+    deep_model = keras.layers.Convolution1D(hidden_filter_num, kernel_size=hidden_filter_size, padding='same')(deep_model)
+    deep_model = keras.layers.BatchNormalization()(deep_model)
+    #deep_model = keras.layers.Activation('relu')(deep_model)
+    deep_model = keras.layers.LeakyReLU()(deep_model)
+    deep_model = keras.layers.MaxPool1D(pool_size=2)(deep_model)
+
+    deep_model = keras.layers.Convolution1D(hidden_filter_num, kernel_size=hidden_filter_size, padding='same')(deep_model)
+    deep_model = keras.layers.BatchNormalization()(deep_model)
+    #deep_model = keras.layers.Activation('relu')(deep_model)
+    deep_model = keras.layers.LeakyReLU()(deep_model)
+    deep_model = keras.layers.MaxPool1D(pool_size=2)(deep_model)
+
+    deep_model = keras.layers.Convolution1D(hidden_filter_num, kernel_size=hidden_filter_size, padding='same')(deep_model)
+    deep_model = keras.layers.BatchNormalization()(deep_model)
+    #deep_model = keras.layers.Activation('relu')(deep_model)
+    deep_model = keras.layers.LeakyReLU()(deep_model)
+    deep_model = keras.layers.MaxPool1D(pool_size=2)(deep_model)
+
+    # deep model - dense layer with dropout 
+    deep_model = keras.layers.Dense(deep_dense_size)(deep_model)
+    deep_model = keras.layers.BatchNormalization()(deep_model)
+    #deep_model = keras.layers.Activation('relu')(deep_model)
+    deep_model = keras.layers.LeakyReLU()(deep_model)
+    deep_model = keras.layers.Dropout(dropout_rate)(deep_model)
+    deep_model = keras.layers.Flatten()(deep_model)
+
+    # shape output only dense layer
+    shape_output = keras.layers.Dense(2, activation='softmax', name='shape_classification')(deep_model)
+
+    # p-value output only dense layer
+    pvalue_output = keras.layers.Dense(2, activation='softmax', name='pvalue_classification')(wide_model)
+
+    # combine wide and deep paths
+    concat = keras.layers.concatenate([wide_model, deep_model, pvalue_output])
+    wide_and_deep = keras.layers.Dense(wide_and_deep_dense_size)(concat)
+    wide_and_deep = keras.layers.BatchNormalization()(wide_and_deep)
+    wide_and_deep = keras.layers.LeakyReLU()(wide_and_deep)
+
+    wide_and_deep = keras.layers.Dense(wide_and_deep_dense_size)(wide_and_deep)
+    wide_and_deep = keras.layers.BatchNormalization()(wide_and_deep)
+    wide_and_deep = keras.layers.LeakyReLU()(wide_and_deep)
+    output = keras.layers.Dense(2, activation='softmax', name='overall_classification')(wide_and_deep)
+    model = keras.models.Model(inputs=[input_deep, input_wide], outputs=[output, shape_output, pvalue_output])
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate), loss=['binary_crossentropy', 'binary_crossentropy', 'binary_crossentropy'], loss_weights=[0.7, 0.3, 0], metrics=['accuracy'])
+    model.load_weights(model_weights_file)
+    return model
